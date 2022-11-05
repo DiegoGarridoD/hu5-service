69b4f0ca95be10505f44bc22ad5ed6ae41c48ca5
Diego Garrido <diego.garrido.d@usach.cl>
2022-11-05 11:02:46 -0300

    Initial commit

---
+++ src/main/java/com/pep2/hu5service/Hu5ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pep2.hu5service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hu5ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hu5ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pep2/hu5service/Hu5ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pep2.hu5service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hu5ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
